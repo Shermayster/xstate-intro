2bd390185ef5e6b203c1dde5ea2b1746e4dc5653
Pavel Shermayster <sherm84@gmail.com>
2020-07-13 18:29:53 +0300

    styles-improvement

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -31,7 +31,7 @@ export const App = () => {
           <button
             id="reset-btn"
             disabled={incrementService.matches("active")}
-            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
+            className={`bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ${
               incrementService.matches("active") &&
               "opacity-50 cursor-not-allowed"
             }`}
